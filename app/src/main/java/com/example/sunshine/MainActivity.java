ce715eb27c46c4d35542a77a85e38d48378d4003
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2016-11-22 16:32:13 +0200

    ForecastFragment added.

--- app/src/main/java/com/example/sunshine/MainActivity.java
+++ app/src/main/java/com/example/sunshine/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.sunshine;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.forecast_fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new ForecastFragment to be placed in the activity layout
+            ForecastFragment forecastFragment = new ForecastFragment();
+
+            // Add the fragment to the 'forecast_fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.forecast_fragment_container, forecastFragment).commit();
+        }
     }
+
 }
