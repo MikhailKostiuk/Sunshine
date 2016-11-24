b019e863a1b02bb72968361bce6b818e3d371cad
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2016-11-24 21:44:41 +0200

    Add AsyncTask for fetching raw forecast data in json format.

--- app/src/main/java/com/example/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/sunshine/ForecastFragment.java
@@ -1,19 +1,30 @@
 package com.example.sunshine;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ForecastFragment extends Fragment {
+
+    public static final String LOG_TAG = ForecastFragment.class.getSimpleName();
 
     private ListView mListView;
     private ArrayAdapter<String> mForecastAdapter;
@@ -40,6 +51,82 @@ public class ForecastFragment extends Fragment {
         mListView.setAdapter(mForecastAdapter);
 
         return rootView;
+    }
+
+    public class FetchWeatherTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader bufferedReader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are available at OWM's forecast API page, at
+                // http://openweathermap.org/API
+                String baseUrl = "http://api.openweathermap.org/data/2.5/forecast/" +
+                        "daily?q=93400&mode=json&units=metric&cnt=7";
+                String apiKey = "&APPID" + BuildConfig.OPEN_WEATHER_MAP_API_KEY;
+                URL url = new URL(baseUrl.concat(apiKey));
+
+                // Create the request to OpenWeatherMap and open the connection.
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String.
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer stringBuffer = new StringBuffer();
+                if (inputStream == null) {
+                    //Nothing to do.
+                    return null;
+                }
+
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    stringBuffer.append(line + "\n");
+                }
+
+                if (stringBuffer.length() == 0) {
+                    // Stream was empty. No point in parsing.
+                    return null;
+                }
+
+                forecastJsonStr = stringBuffer.toString();
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error", e);
+                // If the code didn't successfully get the weather data, there's no point in attempting
+                // to parse it
+                return null;
+
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (bufferedReader != null) {
+                    try {
+                        bufferedReader.close();
+                    } catch (IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            return null;
+        }
     }
 
 }
