421ac9298f0cb6746a7cbd37d69466fdefdac356
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2016-11-26 18:30:03 +0200

    Add internet permission. Refresh button now actually execute FetchWeatheTask and MainActivity extends ACA for having action bar

--- app/src/main/java/com/example/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/sunshine/ForecastFragment.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -64,6 +65,21 @@ public class ForecastFragment extends Fragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_refresh) {
+
+            FetchWeatherTask fetchWeatherTask = new FetchWeatherTask();
+            fetchWeatherTask.execute();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public class FetchWeatherTask extends AsyncTask<Void, Void, Void> {
